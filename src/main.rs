05326e508a6fe2c05914e88b1c3bcedbc0024bea
ThOfficialCoder <sehzademar@outlook.com>
2025-08-25 14:26:52 -0400

    Added enum operators and calculate function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+enum Operators {
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+    Modulo,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let result = calculate(Operators::Add, 10.0, 10.0);
+    println!("{}", result);
+}
+
+fn calculate(operator: Operators, firstNum: f32, secondNum: f32) -> f32 {
+    match operator {
+        Operators::Add => firstNum + secondNum,
+        Operators::Subtract => firstNum - secondNum,
+        Operators::Multiply => -firstNum * secondNum,
+        Operators::Divide => firstNum / secondNum,
+        Operators::Modulo => firstNum % secondNum,
+    }
 }
